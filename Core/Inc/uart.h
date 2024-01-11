b3d79cb5b64ff361594f8ea2fd2fa7f42dcc419d
seungyynn <susan63177@khu.ac.kr>
2024-01-11 16:10:37 +0900

    이것은 첫번째 pwm프로젝트임

---
+++ Core/Inc/uart.h
@@ -0,0 +1,16 @@
+/*
+ * uart.h
+ *
+ *  Created on: Jan 10, 2024
+ *      Author: user
+ */
+
+#ifndef INC_UART_H_
+#define INC_UART_H_
+
+#include "main.h"
+
+//변수 프로토타입 선언
+void initUart(UART_HandleTypeDef *);	//UART 장치 초기화
+
+#endif /* INC_UART_H_ */
